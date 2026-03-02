3c97f303f57f82216076b271a5af06c5a43e8d5e
Marat Timofeev <marat.tmv1@gmail.com>
2026-03-02 13:25:18 +0700

    Сделал компонент ui-box

---
+++ app/components/ui/box.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+defineProps<{
+  title: string
+}>()
+</script>
+
+<template lang="pug">
+div(class="mb-5 rounded-xl border border-gray-200 bg-white p-6 shadow-md")
+  h2(v-if="title" class="mt-0 mb-5 border-b border-gray-100 pb-3 text-xl text-gray-900") {{ title }}
+  div
+    slot
+</template>
